f8ba8e783bec05d17dbe5327bb17fb4dbb58c0be
AlenaMikalaeynaLisava <alenalisava87@gmail.com>
2021-01-22 10:59:17 +0300

    feat: add some styles

--- script.js
+++ script.js
@@ -1,42 +1,50 @@
 const firstRow = {
     view:"layout",
-    gravity: 1,
     css:"webix_dark",
     cols:[
         {view:"button",
 			id:"mybutton",
 			value:"My App",
-			css:"webix_transparent",//color of letters
+			css:"header_button"
         },
         {},
-        { view:"label", label:"<span class='something'><span class='webix_icon wxi-user'></span> Profile</span>", css:"material"},
-    ]
+        { view:"label", label:"<span class='my_span'><span class='webix_icon wxi-user'></span> Profile</span>", css:"material my_label"},
+    ],
+    gravity: 1
 }
 const secondRow = {
-    fillspace:true,
-    autoheight: true,
     cols:[
-        {
+        {gravity: 2,
+            css: "left_bar",
             rows:[
             { view:"list",
-            autoheight: true ,
+            scroll:false,
             id:"mylist",
-            data:[ "Dashboard", "Users", "Products", "Locations"]
+            data:[ "Dashboard", "Users", "Products", "Locations"],
+            css:"webix_list webix_list_item"
             },
-            {},
-            {view:"label", label:"<span class='something1'><span class='webix_icon wxi-check'></span> Connectad</span>", css:"material"}
+            {view:"label", label:"<span class='my_span1'><span class='webix_icon wxi-check'></span> Connected</span>", css:"material, label_text-staff"}
         ]},
+        { view:"resizer" },
         {
-            view:"datatable",
-            autoheight:true,
-            scrollY:false,
-            autoConfig:true,
-            data:small_film_set
+            gravity: 9,
+            scrollY:true,
+            rows:[
+        {view:"datatable",
+        minHeight:450,
+        autoConfig:true,
+        data:small_film_set,
+        scrollX:false},
+        {}
+        ]
+            
           },
-          {
+          {gravity: 4,
             view:"form", 
             autoheight: true ,
+            
             elements:[
+                { view:"template", template:"edit films", type:"section" },
                 { view:"text", label:"Title" },
                 { view:"text", label:"Year" },
                 { view:"text", label:"Rating" },
@@ -47,14 +55,19 @@ const secondRow = {
                 ]}
             ]
         }
-    ]
+    ],
+    minHeight:450,
+    gravity: 15
 }
 const thirdRow = {
-    template: "html->my_box1", autoheight: true,
-    gravity: 1
+    template: "html->my_box1",
+    height: 40,
+    gravity: 1,
+    css:"template",
+    type:"line"
 }
 
 webix.ui({
-    container:"box",
     rows:[ firstRow, secondRow, thirdRow]
 });
+// webix.ui.fullScreen();
