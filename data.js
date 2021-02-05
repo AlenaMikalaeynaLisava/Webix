c33f8d3d7e09ab2df28b2e9ce70e9c3e857f59fd
AlenaMikalaeynaLisava <alenalisava87@gmail.com>
2021-02-05 15:27:06 +0300

    fix: change collection source

--- data.js
+++ data.js
@@ -1,18 +1,10 @@
-var options =[
-	{ "id":1, "value":"Drama" },
-	{ "id":2, "value":"Fiction" },
-	{ "id":3, "value":"Comedy" },
-	{ "id":4, "value":"Horror" }
-]
-
-
- export let data = {view:"datatable",  id:"newdatatable", select:true,
+export let data = {view:"datatable",  id:"newdatatable", select:true,
 hover:"myhover",
 columns:[
  {id:"rank", header:"",  css:{"background":"#F4F5F9"}},
  {id:"title", header:["Film Title", {content:"textFilter"}], editor:"text", fillspace: true, sort:"string_strict"},
  {id:"categoryId",	header:["Category",{content:"textFilter"}], 
- collection:options, editor:"text"
+ collection:"categories.js", editor:"text"
 },
 {id:"rating", header:["Rating", {content:"textFilter"}], sort:"int"},
  {id:"votes", header:["Votes", {content:"textFilter"}], sort:"int",
